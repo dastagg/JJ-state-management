ad486118c8e11ab1045b4871b97063065ed8fda6
dastagg:opus5 <dastagg@stagglinux.com>
2022-03-19 18:53:53 -0400

    Step 2: Pass data up the Widget Tree by passing a function through the widget tree.

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainPage extends StatelessWidget {
-  // final String data = 'John Rambo';
-  final String data = 'Chuck Norris';
+class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  // final String data = 'John Rambo';
+  String data = 'Chuck Norris';
+
+  void changeValue(String dataChanged) {
+    setState(() {
+      data = dataChanged;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print('Building MainPage');
@@ -26,38 +39,59 @@ class MainPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(data),
       ),
-      body: Screen2(data: data),
+      body: Screen2(
+        data: data,
+        changeData: changeValue,
+      ),
     );
   }
 }
 
 class Screen2 extends StatelessWidget {
   final String data;
-  const Screen2({required this.data, Key? key}) : super(key: key);
+  final Function(String) changeData;
+  const Screen2({required this.changeData, required this.data, Key? key})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     print('Building Screen2');
     return Container(
-      child: Screen3(data: data),
+      child: Screen3(
+        data: data,
+        changeData: changeData,
+      ),
     );
   }
 }
 
 class Screen3 extends StatelessWidget {
   final String data;
-  const Screen3({required this.data, Key? key}) : super(key: key);
+  final Function(String) changeData;
+  const Screen3({required this.changeData, required this.data, Key? key})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     print('Building Screen3');
     return Container(
-      child: Screen4(data: data),
+      child: Screen4(
+        data: data,
+        changeData: changeData,
+      ),
     );
   }
 }
 
-class Screen4 extends StatelessWidget {
+class Screen4 extends StatefulWidget {
   final String data;
-  const Screen4({required this.data, Key? key}) : super(key: key);
+  final Function(String) changeData;
+  const Screen4({required this.changeData, required this.data, Key? key})
+      : super(key: key);
+
+  @override
+  State<Screen4> createState() => _Screen4State();
+}
+
+class _Screen4State extends State<Screen4> {
   @override
   Widget build(BuildContext context) {
     print('Building Screen4');
@@ -65,9 +99,13 @@ class Screen4 extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(data),
+          Text(widget.data),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              setState(() {
+                widget.changeData('Bruce Lee');
+              });
+            },
             child: const Text('Change data'),
           ),
         ],
